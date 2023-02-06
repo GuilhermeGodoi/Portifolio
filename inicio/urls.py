e1942a8efedaf6814125426fde37f7d8884f1586
GuilhermeGodoi <guilhermegodoibarreiros2@gmail.com>
2023-02-06 11:28:06 -0300

    Submit Page, Defaut Header and Footer

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from inicio.views import index
+from inicio.views import index, email
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    path('email/', email, name='email'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'inicio/index.html')
+
+def email(request):
+    return render(request, 'inicio/email.html')
